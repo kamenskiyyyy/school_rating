63ab0542dbdf1b61e033041f0bba85d6e8f77f74
kamenskiyyyy <72261703+kamenskiyyyy@users.noreply.github.com>
2021-03-19 15:36:55 +0300

    add loading animate

--- src/panels/Junior.js
+++ src/panels/Junior.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import s from './style.module.css';
 
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
@@ -11,8 +12,17 @@ const Junior = props => (
 			left={<PanelHeaderBack onClick={props.go} data-to="home"/>} >
 			Младшая лига
 		</PanelHeader>
-		<iframe width="584" height="311" seamless frameBorder="0" scrolling="no"
-	src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRbETU57bEGSLFk6sp5q04IuuJwotXahurL5H6c9ZAJLgNVVO8c9rXWRNW1labOPgrROtkYKhXvw4AL/pubchart?oid=1836155378&amp;format=interactive"/>
+		<div className={s.div}>
+			<iframe width="584" height="311" seamless frameBorder="0" scrolling="no"
+					src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRbETU57bEGSLFk6sp5q04IuuJwotXahurL5H6c9ZAJLgNVVO8c9rXWRNW1labOPgrROtkYKhXvw4AL/pubchart?oid=1836155378&amp;format=interactive"/>
+			<div className={s.lds_ellipsis}>
+				<div/>
+				<div/>
+				<div/>
+				<div/>
+			</div>
+		</div>
+
 	</Panel>
 );
 
--- src/panels/Premier.js
+++ src/panels/Premier.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import PanelHeaderBack from '@vkontakte/vkui/dist/components/PanelHeaderBack/PanelHeaderBack';
+import s from "./style.module.css";
 
 const Premier = props => (
     <Panel id={props.id}>
@@ -11,8 +12,16 @@ const Premier = props => (
             left={<PanelHeaderBack onClick={props.go} data-to="home"/>} >
             Премьер лига
         </PanelHeader>
-        <iframe width="584" height="311" seamless frameBorder="0" scrolling="no"
-    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRbETU57bEGSLFk6sp5q04IuuJwotXahurL5H6c9ZAJLgNVVO8c9rXWRNW1labOPgrROtkYKhXvw4AL/pubchart?oid=502916854&amp;format=interactive"/>
+                <div className={s.div}>
+            <iframe width="584" height="311" seamless frameBorder="0" scrolling="no"
+                    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRbETU57bEGSLFk6sp5q04IuuJwotXahurL5H6c9ZAJLgNVVO8c9rXWRNW1labOPgrROtkYKhXvw4AL/pubchart?oid=502916854&amp;format=interactive"/>
+            <div className={s.lds_ellipsis}>
+                <div/>
+                <div/>
+                <div/>
+                <div/>
+            </div>
+        </div>
     </Panel>
 );
 
