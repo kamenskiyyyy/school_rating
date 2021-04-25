c13b6183b625a3c96f2b0dc7e281360bb7f088f8
Kamenev Nikolay <79969997909@mail.ru>
2021-04-25 09:40:54 +0300

    new spinner

--- src/panels/Junior.js
+++ src/panels/Junior.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import s from './style.module.css';
-
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import PanelHeaderBack from '@vkontakte/vkui/dist/components/PanelHeaderBack/PanelHeaderBack';
+import {Spinner} from "@vkontakte/vkui";
 
 const Junior = props => (
 	<Panel id={props.id}>
@@ -13,14 +13,10 @@ const Junior = props => (
 			Младшая лига
 		</PanelHeader>
 		<div className={s.div}>
-			<iframe width="584" height="311" seamless frameBorder="0" scrolling="no"
+			<iframe style={{zIndex: '10'}} width="584" height="311" seamless frameBorder="0" scrolling="no"
 					src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRbETU57bEGSLFk6sp5q04IuuJwotXahurL5H6c9ZAJLgNVVO8c9rXWRNW1labOPgrROtkYKhXvw4AL/pubchart?oid=1836155378&amp;format=interactive"/>
-			<div className={s.lds_ellipsis}>
-				<div/>
-				<div/>
-				<div/>
-				<div/>
-			</div>
+			<Spinner size="large" style={{ margin: '20% 0' }} />
+
 		</div>
 
 	</Panel>
--- src/panels/Premier.js
+++ src/panels/Premier.js
@@ -5,29 +5,25 @@ import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import PanelHeaderBack from '@vkontakte/vkui/dist/components/PanelHeaderBack/PanelHeaderBack';
 import s from "./style.module.css";
+import {Spinner} from "@vkontakte/vkui";
 
 const Premier = props => (
-    <Panel id={props.id}>
-        <PanelHeader
-            left={<PanelHeaderBack onClick={props.go} data-to="home"/>} >
-            Премьер лига
-        </PanelHeader>
-                <div className={s.div}>
-            <iframe width="584" height="311" seamless frameBorder="0" scrolling="no"
-                    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRbETU57bEGSLFk6sp5q04IuuJwotXahurL5H6c9ZAJLgNVVO8c9rXWRNW1labOPgrROtkYKhXvw4AL/pubchart?oid=502916854&amp;format=interactive"/>
-            <div className={s.lds_ellipsis}>
-                <div/>
-                <div/>
-                <div/>
-                <div/>
-            </div>
-        </div>
-    </Panel>
+  <Panel id={props.id}>
+    <PanelHeader
+      left={<PanelHeaderBack onClick={props.go} data-to="home"/>}>
+      Премьер лига
+    </PanelHeader>
+    <div className={s.div}>
+      <iframe style={{zIndex: '10'}} width="584" height="311" seamless frameBorder="0" scrolling="no"
+              src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRbETU57bEGSLFk6sp5q04IuuJwotXahurL5H6c9ZAJLgNVVO8c9rXWRNW1labOPgrROtkYKhXvw4AL/pubchart?oid=502916854&amp;format=interactive"/>
+      <Spinner size="large" style={{margin: '20% 0'}}/>
+    </div>
+  </Panel>
 );
 
 Premier.propTypes = {
-    id: PropTypes.string.isRequired,
-    go: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  go: PropTypes.func.isRequired,
 };
 
 export default Premier;
